3a6682854d8eaabc7ddaf45f5aae36a130177697
Mikail Muzakki <mikailmmuzakki@gmail.com>
2023-11-09 15:34:36 +0700

    feat: add constructor and getters

--- src/main/java/com/Employee.java
+++ src/main/java/com/Employee.java
@@ -8,4 +8,21 @@ public class Employee {
     private String username;
     private String password;
 
+    public Employee(UUID employeeID, String name, String username, String password) {
+        this.employeeID = employeeID;
+        this.name = name;
+        this.username = username;
+    }
+
+    public UUID getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
