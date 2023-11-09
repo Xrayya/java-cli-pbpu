69a19cac2c93c974225f28a5cb05cde77c667875
Mikail Muzakki <mikailmmuzakki@gmail.com>
2023-11-09 15:07:07 +0700

    feat: add actor classes

---
+++ src/main/java/com/Cashier.java
@@ -0,0 +1,5 @@
+package com;
+
+public class Cashier extends Employee{
+
+}
---
+++ src/main/java/com/Employee.java
@@ -0,0 +1,11 @@
+package com;
+
+import java.util.UUID;
+
+public class Employee {
+    private UUID employeeID;
+    private String name;
+    private String username;
+    private String password;
+
+}
