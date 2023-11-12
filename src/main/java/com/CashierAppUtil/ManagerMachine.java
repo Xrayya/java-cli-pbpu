fe2c1078879d5d3b0c5b674a2161737fd93e4d43
Xrayya <nur.alkhiyar@gmail.com>
2023-11-12 18:01:22 +0700

    refactor: change data type and simplify logic

--- src/main/java/com/CashierAppUtil/ManagerMachine.java
+++ src/main/java/com/CashierAppUtil/ManagerMachine.java
@@ -10,49 +10,35 @@ public class ManagerMachine extends CashierMachine {
     /**
      * Add menu to menu list
      * @param menu menu to be added
+     * @return `true` if menu succesfully added or `false` if not
      */
-    public void addMenu(Menu menu) {
-        // TODO: implement this method
-        // NOTE: manipulasi object menus pada class ini untuk menambahkan menu dari
-        // parameter method ini
-        this.menus.add(menu);
-        System.out.println("Berhasil menambahkan menu");
+    public boolean addMenu(Menu menu) {
+        return this.menus.add(menu);
     }
 
     /**
      * Remove menu from menu list
      * @param menu menu to be removed from the list
+     * @return `true` if the specified menu found or `false` if not found
      */
-    public void removeMenu(Menu menu) {
-        // TODO: implement this method
-        // NOTE: manipulasi object menus pada class ini untuk menghapus menu dari
-        // parameter method ini
-        for(int i = 0; i < this.menus.size(); i++) {
-            if(menu.getMenuId() == this.menus.get(i).getMenuId()) {
-                this.menus.remove(i);
-                System.out.println("Berhasil menghapus menu yang dipilih");
-                return;
-            }
-        }
-        System.out.println("Menu yang kamu masukkan tidak terdaftar!");
+    public boolean removeMenu(Menu menu) {
+        return this.menus.remove(menu);
     }
 
     /**
      * Edit menu in the menu list
      * @param menuId menuId from menu that to be edited
      * @param menu edited menu
+     * @return `true` if edit the specified menuId found or `false` if not found
      */
-    public void editMenu(UUID menuId, Menu menu) {
-        // TODO: implement this method
-        // NOTE: manipulasi object menus pada class ini untuk mengedit menu dengan menuId dari
-        // parameter method ini kemudian ubah isinya sesuai dengan menu pada parameter kedua
+    public boolean editMenu(UUID menuId, Menu menu) {
         for(int i = 0; i < this.menus.size(); i++) {
             if(menuId == this.menus.get(i).getMenuId()) {
                 this.menus.set(i, menu);
-                System.out.println("Berhasil mengedit menu yang dipilih");
-                return;
+                return true;
             }
         }
-        System.out.println("Menu yang kamu pilih tidak terdaftar!");
+
+        return false;
     }
 }
