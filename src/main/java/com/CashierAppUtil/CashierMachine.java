e7fb72e7398f0a133f723b6d79548649cbb47bf3
Xrayya <nur.alkhiyar@gmail.com>
2023-11-13 15:01:28 +0700

    feat: complete CashierMachine feature

--- src/main/java/com/CashierAppUtil/CashierMachine.java
+++ src/main/java/com/CashierAppUtil/CashierMachine.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import com.RecordUtil.Log;
 import com.RecordUtil.Record;
 
 /**
@@ -15,8 +16,8 @@ public class CashierMachine {
     protected List<Menu> menus;
 
     public CashierMachine() {
-        Record<Menu> menuRecord = new Record<>("menus");
-        this.menus = menuRecord.readRecordFile();
+        this.menus = new ArrayList<>();
+        this.menus.addAll(new Record<Menu>("menus", Menu[].class).readRecordFile());
         this.orders = new ArrayList<>();
     }
 
@@ -24,9 +25,11 @@ public class CashierMachine {
      * Print all menu in the record
      */
     public void printMenu() {
-        for (Menu m : this.menus) {
-            System.out.printf("%-20s %s\n", m.getMenuName(), m.getPrice());
+        for (Menu menu : this.menus) {
+            System.out.println("-".repeat(50));
+            System.out.println(menu.toString());
         }
+        System.out.println("-".repeat(50));
     }
 
     /**
@@ -39,7 +42,6 @@ public class CashierMachine {
             return false;
         }
         this.orders.add(order);
-        System.out.println("Order berhasil ditambahkan");
         return true;
     }
 
@@ -52,17 +54,15 @@ public class CashierMachine {
      * 
      * @param order order to be removed from the list of orders
      */
-    public boolean removeOrder(UUID ordeUuid) {
-        if (checkNull(ordeUuid)) {
+    public boolean removeOrder(UUID orderId) {
+        if (checkNull(orderId)) {
             return false;
         }
-        int findOrder = findOrder(ordeUuid);
+        int findOrder = findOrder(orderId);
         if (findOrder == -1) {
-            System.out.println("Order tidak ditemukan");
             return false;
         }
         this.orders.remove(findOrder);
-        System.out.println("Order berhasil dihapus");
         return true;
     }
 
@@ -75,17 +75,15 @@ public class CashierMachine {
     public boolean editOrder(UUID orderId, Order order) {
         int indexFound = findOrder(orderId);
         if (indexFound == -1) {
-            System.out.println("Order tidak ditemukan");
             return false;
         }
         this.orders.set(indexFound, order);
-        System.out.println("Order berhasil diupdate");
         return true;
     }
 
-    private int findOrder(UUID ordeUuid) {
+    private int findOrder(UUID orderId) {
         for (int i = 0; i < this.orders.size(); i++) {
-            if (this.orders.get(i).getOrderID().equals(ordeUuid)) {
+            if (this.orders.get(i).getOrderId().equals(orderId)) {
                 return i;
             }
         }
@@ -98,24 +96,23 @@ public class CashierMachine {
      * @param order order to be printed in struct
      */
     public void printStruct(Order order) {
-        checkNull(order);
-        System.out.printf("%-20s %-20s %s\n", "ORDER ID", "TABLE NUMBER", "TOTAL PRICE");
-        System.out.printf("%-20s %-20s %s\n", order.getOrderID(), order.getTableNumber(), order.getTotalPrice());
-        System.out.println("====================== ORDERS =======================");
-        System.out.printf("%-20s %-20s %-20s %s\n", "MENU NAME", "PRICE", "QUANTITY", "TOTAL PRICE");
-        for (MenuOrder mo : order.getMenuOrders()) {
-            System.out.printf("%-20s %-20s %s\n", mo.getMenu().getMenuName(), mo.getMenu().getPrice(), mo.getQuantity(),
-                    mo.getTotalPrice());
-        }
-
+        // checkNull(order);
+        // System.out.printf("%-20s %-20s %s\n", "ORDER ID", "TABLE NUMBER", "TOTAL PRICE");
+        // System.out.printf("%-20s %-20s %s\n", order.getOrderID(), order.getTableNumber(), order.getTotalPrice());
+        // System.out.println("====================== ORDERS =======================");
+        // System.out.printf("%-20s %-20s %-20s %s\n", "MENU NAME", "PRICE", "QUANTITY", "TOTAL PRICE");
+        // for (MenuOrder mo : order.getMenuOrders()) {
+        //     System.out.printf("%-20s %-20s %s\n", mo.getMenu().getMenuName(), mo.getMenu().getPrice(), mo.getQuantity(),
+        //             mo.getTotalPrice());
+        // }
+        System.out.println(order.toString());
     }
 
     /**
      * Save the current order list to the record file
      */
     public void saveToRecord() {
-        // NOTE: gunakan class `Log` untuk menyimpan data orders ke record
-        checkNull(this.orders);
-        // TODO: write record
+        Log<Order> orderLog = new Log<>("orders", Order[].class);
+        orderLog.appendRecord(this.orders);
     }
 }
