29080cc223b2c9aaeab4e0bc5a6a3d552422ace9
Xrayya <nur.alkhiyar@gmail.com>
2023-11-13 14:28:51 +0700

    merge feat/8-develop-log-and-record

--- src/main/java/com/RecordUtil/Log.java
+++ src/main/java/com/RecordUtil/Log.java
@@ -1,24 +1,72 @@
 package com.RecordUtil;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Log is class to access record file. The record is writable
  */
 public class Log<T> extends Record<T> {
 
-    public Log(String recordName, String recordFile) {
-        super(recordName);
+    public Log(String recordFileName, Class<T[]> modelClass) {
+        super(recordFileName, modelClass);
     }
 
     /**
      * Write record to record file
      * 
-     * @param record record to be written to record file 
+     * @param record record to be written to record file
      * @return `true` if writing operation is succesful or `false` if the writing
      *         operation failed
      */
-    public boolean writeRecord(T record) {
-        // TODO: implement this method
-        // NOTE: tuliskan record dari parameter ke file record
+    public boolean appendRecord(T record) {
+        List<T> recordList = new ArrayList<>();
+        recordList.addAll(this.readRecordFile());
+        recordList.add(record);
+        String s = this.gson.toJson(recordList);
+        try {
+            FileWriter fileWriter = new FileWriter(this.recordFile);
+            fileWriter.write(s);
+            fileWriter.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean appendRecord(List<T> record) {
+        List<T> recordList = new ArrayList<>();
+        recordList.addAll(this.readRecordFile());
+        System.out.println(recordList);
+        recordList.addAll(record);
+        String s = this.gson.toJson(recordList);
+        try {
+            FileWriter fileWriter = new FileWriter(this.recordFile);
+            fileWriter.write(s);
+            fileWriter.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean rewriteRecord(T record) {
+        List<T> recordList = new ArrayList<>();
+        recordList.add(record);
+        String s = this.gson.toJson(recordList);
+        try {
+            FileWriter fileWriter = new FileWriter(this.recordFile);
+            fileWriter.write(s);
+            fileWriter.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
