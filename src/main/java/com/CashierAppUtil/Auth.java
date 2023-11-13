74d43ba69021295a76be4710b6e53fc1fc63f9dd
Xrayya <nur.alkhiyar@gmail.com>
2023-11-13 22:40:56 +0700

    fix: Auth enable to differentiate employee type

--- src/main/java/com/CashierAppUtil/Auth.java
+++ src/main/java/com/CashierAppUtil/Auth.java
@@ -4,17 +4,25 @@ import java.util.List;
 
 import com.Model.Cashier;
 import com.Model.Employee;
+import com.Model.Manager;
 import com.RecordUtil.Record;
 
 /**
  * Auth
  */
 public class Auth {
-    private static final List<Employee> employeesRecord = new Record<Employee>("employees", Employee[].class).readRecordFile();
     public static Employee authenticate(String username, String password) {
-        for (Employee employee : employeesRecord) {
+        List<Employee> employees = new Record<Employee>("managers", Employee[].class).readRecordFile();
+        for (Employee employee : employees) {
             if (employee.getUsername().equals(username) && employee.getPassword().equals(password)) {
-                return employee;
+                return new Manager(employee);
+            }
+        }
+
+        employees = new Record<Employee>("cashiers", Employee[].class).readRecordFile();
+        for (Employee employee : employees) {
+            if (employee.getUsername().equals(username) && employee.getPassword().equals(password)) {
+                return new Cashier(employee);
             }
         }
         return null;
