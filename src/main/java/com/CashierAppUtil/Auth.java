6e84f0570da021cc5c6d05553cbd35f38abf8c7d
putubhanuartha <putuaryabhanu03@gmail.com>
2023-11-13 12:43:27 +0700

    feat/ authentication & authorization done

--- src/main/java/com/CashierAppUtil/Auth.java
+++ src/main/java/com/CashierAppUtil/Auth.java
@@ -3,19 +3,25 @@ package com.CashierAppUtil;
 import java.util.List;
 
 import com.Model.Employee;
+import com.RecordUtil.Record;
 
 public class Auth {
     public final static String managerType = "manager";
     public final static String cashierType = "cashier";
     private static List<Employee> listEmployee;
-    Auth(){
-        
-    }
-    public Employee authentication(String username, String password) {
-        for(Employee e : Auth.listEmployee){
+    private Record<Employee> recordEmployee = new Record<>("employee", Employee[].class);
 
+    Auth() {
+        Auth.listEmployee = recordEmployee.readRecordFile();
+    }
+
+    public Employee authentication(String username, String password) {
+        for (Employee e : Auth.listEmployee) {
+            if(e.getUsername().equals(username) && e.getPassword().equals(password)){
+                return e;
+            }
         }
-        return new Employee(null, username, username, password);
+        return null;
     }
 
     public CashierMachine authorization(String employeeType) {
